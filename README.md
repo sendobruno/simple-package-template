1e7ceabad4edbbc2a62854d4ee78e5c45644956b
sendobruno <brunoaraujoalmeida@gmail.com>
2024-10-30 13:33:23 -0300

    feat: add resolução desafio package

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
-# package_name
+# hello-world-baalmeida
 
 Description. 
-The package package_name is used to:
-	- 
-	-
+The package hello_world is used to:
+	- Package use demonstration
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install hello-world-baalmeida
 
 ```bash
-pip install package_name
+pip install hello-world-baalmeida
 ```
 
-## Usage
+<!-- ## Usage
 
 ```python
-from package_name import file1_name
+from hello_world import file1_name
 file1_name.my_function()
-```
+``` -->
 
 ## Author
-My_name
+Bruno Almeida
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="hello-world-baalmeida",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Bruno Almeida",
+    # author_email="my_email",
+    description="Demonstrando uso de pacotes.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/sendobruno/simple-package-template",
     packages=find_packages(),
-    install_requires=requirements,
+    # install_requires=requirements,
     python_requires='>=3.8',
 )
